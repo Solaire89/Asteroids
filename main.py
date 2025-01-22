785079c5634ad1bb1aa87a662822aa766388829c
ThePrimeagen <the.primeagen@aol.com>
2025-01-22 16:53:30 -0600

    A: Set up basic window with constants file and requirements.txt

---
+++ main.py
@@ -0,0 +1,29 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame # type: ignore
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+
+"""
+This line ensures the main() function is only called when this file is run directly;
+it won't run if it's imported as a module. 
+It's considered the "pythonic" way to structure an executable program in Python. 
+Technically, the program will work fine by just calling main(), 
+but you might get an angry letter from Guido van Rossum if you don't.
+"""
