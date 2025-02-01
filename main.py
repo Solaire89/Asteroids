32e90ce0691c73ca6db3bb74295c13faeb7ecca2
Solaire89 <tech_raymond_j_mccormick@hotmail.com>
2025-01-31 19:01:25 -0600

    Got rid of last comment in main.py

--- main.py
+++ main.py
@@ -56,11 +56,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-"""
-This line ensures the main() function is only called when this file is run directly;
-it won't run if it's imported as a module. 
-It's considered the "pythonic" way to structure an executable program in Python. 
-Technically, the program will work fine by just calling main(), 
-but you might get an angry letter from Guido van Rossum if you don't.
-"""
